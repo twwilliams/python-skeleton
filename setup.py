b702f9a1d8d785860c9377c3d71fb6b762cf17db
twwilliams <tommy@sapid.com>
2017-11-15 20:54:20 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Tommy Williams',
+    'url': 'URL to retrieve it',
+    'download_url': 'Where to download it',
+    'author_email': 'tommy@sapid.com'
+    'version': '0.1',
+    'install_requires': ['pytest'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
